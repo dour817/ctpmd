3f1bca246665eec2911cf2215cbc7e6888f9da73
dour817 <tangchunze@shanlinjinrong.com>
2018-03-19 11:55:56 +0800

    加入启动和监控ctpmd的py脚本

--- ctpmd/Release/monitor_ctpmd.py
+++ ctpmd/Release/monitor_ctpmd.py
@@ -5,16 +5,17 @@ import sys
 import psutil
 import traceback
 import threading
+
 '''
 行情接收程序部署说明：
 可执行文件 ctpmd 为行情接收程序，本py文件是定时启动和关闭，并且监控ctpmd进程运行的脚本程序。
 1,首先将Release文件夹下的所有.so文件复制到/usr/local/lib/文件夹下，然后运行ldconfig。若不想复制到/usr/local/lib/，也可以运行export LD_LIBRARY_PATH=.so文件所有在路径
 将动态库查找的路径加入到LD_LIBRARY_PATH环境变量。
-2,运行cmd : python3 本监控py脚本  ctpmd可执行文件所在路径 例如如下
+2,运行cmd : python3 本监控py脚本  ctpmd可执行文件所在路径    例如如下命令即可启动行情程序。每日8：50和20：50 启动。 15：15和02：45关闭，
           python3 /home/tcz/monitor_ctpmd.py /home/tcz/learngit/ctpmd/Release/
   另外需要安装psutil包。
 '''
-#
+
 def main():
     '''
     根据交易时间，启动和杀掉行情接收程序
